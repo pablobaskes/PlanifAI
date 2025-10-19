ec29754650c99f878da1196c99c9e2e100d5054e
Pablo <pablovazquezbarrena97@gmail.com>
2025-10-20 00:50:12 +0200

    refactored PantryItemService

--- diet/diet-service/src/main/java/com/planifAI/diet_service/service/PantryItemService.java
+++ diet/diet-service/src/main/java/com/planifAI/diet_service/service/PantryItemService.java
@@ -2,6 +2,7 @@ package com.planifAI.diet_service.service;
 
 import com.planifAI.diet_service.dto.PantryItemDTO;
 import com.planifAI.diet_service.mapper.PantryItemMapper;
+import com.planifAI.diet_service.model.Ingredient;
 import com.planifAI.diet_service.model.PantryItem;
 import com.planifAI.diet_service.repository.PantryItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +13,16 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class PantryService {
+public class PantryItemService {
 
     private final PantryItemRepository pantryRepository;
     private final PantryItemMapper pantryMapper;
+
+    public PantryItemDTO findById(UUID id) {
+        return pantryRepository.findById(id)
+                .map(pantryMapper::toDto)
+                .orElseThrow(() -> new RuntimeException("Ingredient not found"));
+    }
 
     public List<PantryItemDTO> findAll() {
         return pantryMapper.toDtoList(pantryRepository.findAll());
@@ -24,6 +31,13 @@ public class PantryService {
     public PantryItemDTO create(PantryItemDTO dto) {
         PantryItem item = pantryMapper.toEntity(dto);
         return pantryMapper.toDto(pantryRepository.save(item));
+    }
+
+    public PantryItemDTO update(UUID id, PantryItemDTO dto) {
+        PantryItem entity = pantryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ingredient not found"));
+        pantryMapper.updateEntityFromDto(dto, entity);
+        return pantryMapper.toDto(pantryRepository.save(entity));
     }
 
     public void delete(UUID id) {
